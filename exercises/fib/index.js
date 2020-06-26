eb4769890703e82fc2843e3b2961c71816c56336
robin-raq <robin.raq@gmail.com>
2020-06-26 18:48:21 -0400

    solved fizzbuzz and fibonnaci using recursion and memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,41 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+//memoization
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
+}
+
+//recursive;
+// function fib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+//   return fib(n - 1) + fib(n - 2);
+// }
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+// function fib(n) {
+//   let fibArr = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     fibArr.push(fibArr[i - 2] + fibArr[i - 1]);
+//   }
+//   return fibArr[n];
+// }
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
