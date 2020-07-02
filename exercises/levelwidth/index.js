aa11383f8b9ccba6a0da83cfab28eaaacf52bfb9
robin-raq <robin.raq@gmail.com>
2020-07-01 21:06:22 -0400

    solved tree level width problem

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,21 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let processing = [root, "s"];
+  let counters = [0];
+  // let i = 0;
+  while (processing.length > 1) {
+    let node = processing.shift();
+    if (node !== "s") {
+      processing.push(...node.children);
+      counters[counters.length - 1] += 1;
+    } else {
+      counters.push(0);
+      processing.push("s");
+    }
+  }
+  return counters;
+}
 
 module.exports = levelWidth;
